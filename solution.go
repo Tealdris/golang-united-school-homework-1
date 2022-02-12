4f96e477a63ca38840d1e9554d66e4ac35f02976
Tealdris <ilyaradomskiy7771@gmail.com>
2022-02-12 10:42:29 +0300

    rework

--- solution.go
+++ solution.go
@@ -1,5 +1,12 @@
 package solution
 
-func GetMessage() string {
-	return ""
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji"
+)
+
+func GetMessage() {
+	pizzaMessage := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(pizzaMessage)
 }
